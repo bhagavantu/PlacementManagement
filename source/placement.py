480aaffc483283792d0d40dd2b1c989ca39fbe93
bhagavantu <49069957+bhagavantu@users.noreply.github.com>
2019-04-02 00:00:05 +0530

    Update placement.py

--- source/placement.py
+++ source/placement.py
@@ -1,6 +1,6 @@
 IT_companies={"Accenture":6.7, "Mindtree":7, "Apple":8.5, "Infosis":8}
-core_companies={"Bosch":7.2, "Sankalp":8, "Siemen":8.5, "Schnieder":7.8}
-companies={"Accenture":6.7, "Mindtree":7, "Apple":8.5, "Infosis":8, "Bosch":7.2, "Sankalp":8, "Siemen":8.5, "Schnieder":7.8}
+core_companies={"Bosch":7.2, "Texas Instruments":8, "Siemens":8.5, "Schnieder":7.8}
+companies={"Accenture":6.7, "Mindtree":7, "Apple":8.5, "Infosis":8, "Bosch":7.2, "Texas Instruments":8, "Siemens":8.5, "Schnieder":7.8}
 class PlacementManagement:
 	"""
 	General Data Class for a place Management
